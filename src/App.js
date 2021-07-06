eabd46c2c0052a32ed9c82e95252b60778f13dd9
Luiz Alexandre Wendel Balbino <alexandre.lawb@gmail.com>
2021-07-06 11:13:27 -0300

    adiciona layout

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
-function App() {
-  return (
-    <div />
-  );
-}
+import React from "react";
+import TitlesList from "./components/TitlesList";
 
-export default App;
+export default class App extends React.Component {
+  render() {
+    return (
+      <main>
+        <h1>Selected: frontend</h1>
+  
+        <select>
+          <option value="frontend" selected>frontend</option>
+          <option value="reactjs">reactjs</option>
+        </select>
+  
+        <span>Las updated at { Date.now() }</span>
+        <input type="button" value="Refresh" />
+
+        <TitlesList />
+      </main>
+    );
+  }
+}
---
+++ src/components/TitlesList.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default class TitlesList extends React.Component {
+  render() {
+    return (
+      <ul>
+        {}
+      </ul>
+    )
+  }
+}
